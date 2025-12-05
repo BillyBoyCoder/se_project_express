543187e24bfead8efd67fe0c4db8a573c3359874
William Cook <abetterlifewitharthur@gmail.com>
2025-12-05 11:29:31 -0500

    set up the MongoDB cloud connection

--- se_project_express-main/app.js
+++ se_project_express-main/app.js
@@ -1,9 +1,20 @@
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 
-const { PORT = 3001 } = process.env;
+const { PORT = 3001, MONGODB_URI } = process.env;
 
 const app = express();
+
+// Connect to MongoDB
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("Connected to MongoDB");
+  })
+  .catch((err) => {
+    console.error("Error connecting to MongoDB:", err);
+  });
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
